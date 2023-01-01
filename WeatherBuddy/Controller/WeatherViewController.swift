28c7ee927dc03373a0b38ee110658866cd8a2c5a
Khiem Truong <khiemtruongxuan@gmail.com>
2023-01-01 17:55:48 +1100

    Initial Commit

---
+++ WeatherBuddy/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherBuddy
+//
+//  Created by Truong Xuan Khiem on 1/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
